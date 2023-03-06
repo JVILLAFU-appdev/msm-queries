4a59fd07e7d655365141d0fcad0ea3bb07a06a10
JVILLAFU <JVILLAFU@CHICAGOBOOTH.EDU>
2023-03-06 17:16:22 +0000

    MSM Queries v.2

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,6 +2,8 @@ class DirectorsController < ApplicationController
 
   def index
 
+    @list_of_directors = Director.all
+    
     render({:template => "director_template/index.html.erb"})
   end
 
